92c54d58ad4039f068c2d3ba0feb1d618d7095d3
fangkyi03 <lvch@ztesa.com.cn>
2019-06-21 19:43:38 +0800

    1.提交

---
+++ lib/input-item/CustomInput.d.ts
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import CustomKeyboard from './CustomKeyboard';
+import { InputEventHandler } from './PropsType';
+export interface NumberInputProps {
+    placeholder?: string;
+    disabled?: boolean;
+    editable?: boolean;
+    moneyKeyboardAlign?: 'left' | 'right' | string;
+    moneyKeyboardWrapProps?: object;
+    moneyKeyboardHeader?: React.ReactNode;
+    value?: string;
+    prefixCls?: string;
+    keyboardPrefixCls?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onFocus?: InputEventHandler;
+    onBlur?: InputEventHandler;
+    onVirtualKeyboardConfirm?: InputEventHandler;
+    confirmLabel: any;
+    backspaceLabel: any;
+    cancelKeyboardLabel: any;
+    maxLength?: number;
+    type?: string;
+    style?: React.CSSProperties;
+    autoAdjustHeight?: boolean;
+}
+declare class NumberInput extends React.Component<NumberInputProps, any> {
+    static defaultProps: {
+        onChange: () => void;
+        onFocus: () => void;
+        onBlur: () => void;
+        onVirtualKeyboardConfirm: () => void;
+        placeholder: string;
+        disabled: boolean;
+        editable: boolean;
+        prefixCls: string;
+        keyboardPrefixCls: string;
+        autoAdjustHeight: boolean;
+    };
+    container: HTMLDivElement;
+    inputRef: HTMLDivElement | null;
+    constructor(props: NumberInputProps);
+    onChange: (value: any) => void;
+    onConfirm: (value: any) => void;
+    componentWillReceiveProps(nextProps: NumberInputProps): void;
+    componentDidUpdate(): void;
+    addBlurListener: () => void;
+    removeBlurListener: () => void;
+    componentWillUnmount(): void;
+    saveRef: (el: CustomKeyboard | null) => void;
+    getComponent(): JSX.Element;
+    getContainer(): HTMLDivElement;
+    renderCustomKeyboard(): void;
+    doBlur: (ev: MouseEvent) => void;
+    removeCurrentExtraKeyboard: () => void;
+    unLinkInput: () => void;
+    onInputBlur: (value: string) => void;
+    onInputFocus: () => void;
+    onKeyboardClick: (KeyboardItemValue: string) => void;
+    onFakeInputClick: () => void;
+    focus: () => void;
+    renderPortal(): JSX.Element | null;
+    render(): JSX.Element;
+}
+export default NumberInput;
